980130a20fbf30064c3220985126fdd83ff974de
Rafael Queiroz <rafaelrq@gmail.com>
2024-11-30 13:58:46 -0300

    Feat:
    Added a state machine to the application and added some handlers for states

--- e-chaplet.ino
+++ e-chaplet.ino
@@ -36,23 +36,90 @@
  * Private Definitions                                                       *
  *****************************************************************************/
 
+/* Pin used to calibrate RF controller is PIN D12 */
+
+#define RF_CTRL_CONFIG                              12
+
+/* Needed amount of time in ms holding the config button to trigger RF
+ * controller calibration
+ */
+
+#define RF_CONFIG_TIMEOUT_MS                        5000UL
+
+enum AppState {
+    APP_STATE_CHAPLET,
+    APP_STATE_RF_CALIBRATION,
+};
+
+
+struct RfOpCodes
+{
+    unsigned long forward;
+    unsigned long backward;
+};
+
 
 /*****************************************************************************
  * Private Function Prototypes                                               *
  *****************************************************************************/
 
+static void checkForRfCalibration();
+
+static void runChaplet();
+static void runRfCalibration();
 
 /*****************************************************************************
  * Private Data                                                              *
  *****************************************************************************/
 
 static RCSwitch mSwitch = RCSwitch();
-
+static bool rfCtrlState;
+static unsigned long pressInstant; 
+static uint8_t appState;
+static struct RfOpCodes rfOpCodes;
 
 /*****************************************************************************
  * Private Functions                                                         *
  *****************************************************************************/
 
+static bool checkForRfCalibration()
+{
+    if (rfCtrlState) {
+        if (digitalRead(RF_CTRL_CONFIG) == HIGH) {
+            rfCtrlState = false;
+        }
+        else if (millis() - pressInstant >= RF_CONFIG_TIMEOUT_MS) {
+            return true;
+        }
+    }
+    else if (digitalRead(RF_CTRL_CONFIG) == LOW) {
+        rfCtrlState  = true;
+        pressInstant = millis();
+    }
+
+    return false;
+}
+
+
+static void runChaplet()
+{
+    if (checkForRfCalibration()) {
+        /* We must put the system in RF calibration mode */
+
+        appState = APP_STATE_RF_CALIBRATION;
+
+        return;
+    }
+
+    /* Do we need to update the chaplet LEDs? */
+
+    if (mSwitch.available()) {
+        /* Yes, we do */
+
+        mSwitch.resetAvailable();
+    }
+
+}
 
 /*****************************************************************************
  * Public Functions                                                          *
@@ -64,21 +131,35 @@ void setup() {
 
     pinMode(LED_BUILTIN, OUTPUT);
     
-    /* initialize serial port for debugging purposes */
+    /* Initialize pin to enable calibration of RF controller */
+
+    pinMode(RF_CTRL_CONFIG, INPUT);
+
+    /* initialize serial port for debugging and logging purposes */
     
     Serial.begin(9600);
     
-    /*  */
+    /* Enable Receiving signals of RF controller on INT line 0 (PIN D2) */
 
     mSwitch.enableReceive(0);
 }
 
 void loop() {
-
-    if (mSwitch.available()) {
-        mSwitch.resetAvailable();
+    
+    switch (appState) {
+        case APP_STATE_CHAPLET:
+            runChaplet();
+            break;
+        case APP_STATE_RF_CALIBRATION:
+            runRfCalibration();
+            break;
+        default:
+            /* We should not get here */
+            
+            Serial.println("Illegal app state. Going back to chaplet state");
+            appState = APP_STATE_CHAPLET;
+            break;
     }
-
 }
 
 
